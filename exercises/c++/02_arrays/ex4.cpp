4e4ba1a401b2f81266a716015b3875e1617d347e
Lorenzo Basile <lbasile14@hotmail.it>
2019-10-16 12:22:13 +0200

    added ex4_lecture2, modified ex3_lecture2

--- exercises/c++/02_arrays/ex4.cpp
+++ exercises/c++/02_arrays/ex4.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
-bool* erathostenes(bool*, unsigned int);
+std::vector<bool> erathostenes(std::vector<bool>, unsigned int);
 
 
 int main(){
     unsigned int num;
     std::cout<<"insert number: ";
-    std::cin>>num;
-    bool* sieve=new bool[num+1];
-    for (int i=2; i<=num; i++)    sieve[i]=true;
+    std::cin>>num;    
+    std::vector<bool> sieve(num+1);
+    for (int i=2; i<=num; i++){
+        sieve[i]=true;
+    }
     sieve=erathostenes(sieve, num);
     std::cout<<std::endl;
     for (int i=2; i<=num; i++){
@@ -17,10 +20,10 @@ int main(){
     }
     return 0;
 }
-bool* erathostenes(bool* vect, unsigned int max){
+std::vector<bool> erathostenes(std::vector<bool> vect, unsigned int max){
     for (int i=2; i<=std::sqrt(max); i++){
         if(vect[i]){
-            for (int j=2*i; j<=max; j +=i) vect[j]=false; 
+            for (int j=2*i; j<=max; j +=i) vect[j]=false;
         }
     }
     return vect;
